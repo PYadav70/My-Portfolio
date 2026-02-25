47e0a9f9071d09dffba6ea3ea7e26221e5e62c76
PYadav70 <technoyadav1234@gmail.com>
2026-02-26 00:13:44 +0530

    Refactor code structure for improved readability and maintainability

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -51,8 +51,8 @@ const Header = () => {
              initial={{y:30, opacity:0}}
             whileInView={{y:0, opacity:1}}
             transition={{duration:0.6, delay:1.2}}
-            href='/my_resume (1).pdf'
-              download="/my_resume (1).pdf" className='px-10 py-3 border rounded-full border-gray-500 flex
+            href='/my_resume.pdf'
+              download="/my_resume.pdf" className='px-10 py-3 border rounded-full border-gray-500 flex
             items-center gap-2 bg-white dark:text-black'>My Resume <Image src={assets.download_icon}
              alt='' className=' w-4 '/></motion.a>
         </div>
